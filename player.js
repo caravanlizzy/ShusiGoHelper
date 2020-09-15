75948396e6c69995445f9948643076d8c128ab9a
caravanlizzy <marc.lambrecht@gmx.de>
2020-09-15 11:08:20 +0200

    added interface

---
+++ player.js
@@ -0,0 +1,9 @@
+function Player(playerNum, text){
+    this.tableau = [];
+    this.name = "";
+    this.infoBoxText = text;
+    this.infoContainer = null;
+    this.cardDisplay = null;
+    this.playerNum = playerNum;
+    this.cards = [];    
+}
